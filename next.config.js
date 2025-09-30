96d1c69e6c44e0df71a5607640ef505eed9c3e3b
mkof14 <mkofman@gmail.com>
2025-09-30 12:36:33 -0400

    chore(next): rewrite /api/_ops/health -> /api/ops/health

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async rewrites() {
+    return [
+      { source: '/api/_ops/health', destination: '/api/ops/health' },
+    ];
+  },
 };
 module.exports = nextConfig;
