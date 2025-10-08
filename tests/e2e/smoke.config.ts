ec8adef326a71f0f9cfb5542203ad79b1e805787
mkof14 <mkofman@gmail.com>
2025-10-07 20:09:51 -0400

    test(smoke): set testDir '.' in smoke.config

--- tests/e2e/smoke.config.ts
+++ tests/e2e/smoke.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "tests/e2e",
+  testDir: ".",
   workers: 1,
   use: { baseURL: "http://127.0.0.1:3010" },
   webServer: {
